c91efca83a50bc34ec737ebb6a5288e729eb8321
Adam Faryna <adamfaryna@gmail.com>
2016-01-21 01:24:18 +0000

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# CSS ZEN Garden with Sass
+
+This is CSS Zen Garden style project using Sass, Gulp and Autoprefixer.
+
+If you don't know what CSS Zen Garden is go to project page http://www.csszengarden.com.
+
+Feel free to use it if you want.
+
+# Licence
+
+This project is licensed under the [MIT licence](LICENSE.md).
+
+# Enjoy!
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var less         = require('gulp-less');
+
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefix= new LessPluginAutoPrefix({ browsers: ["last 2 versions"] });
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['less'], function() {
+  browserSync.init({
+    browser: 'google chrome',
+    server: './app',
+    port: '3000'
+  });
+
+  gulp.watch('less/*.less', ['less']);
+  gulp.watch('app/index.html').on('change', browserSync.reload);
+});
+
+// Compile less into CSS & auto-inject into browsers
+gulp.task('less', function() {
+  return gulp.src('less/style.less')
+    .pipe(less({ plugins: [autoprefix, cleancss] }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
